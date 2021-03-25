5889ba2f038df4a9c6d33b45f5e6189a9442ae4d
Sara Viray <74330486+snviray@users.noreply.github.com>
2021-03-24 20:58:37 -0700

    added health bar and directionality with shift

---
+++ Tile Turn-Based Party Project/Assets/Scripts/FillStatusBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillStatusBar : MonoBehaviour
+{
+    public Character player;
+    public Image fillImage;
+    private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        player = PlayerManager.singleton.gameObject.GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float fillValue = (float) player.currentHealth / player.totalHealth;
+
+
+        slider.value = fillValue;
+        if (fillValue <= 0) {
+            fillImage.enabled = false;
+        }
+    }
+}
