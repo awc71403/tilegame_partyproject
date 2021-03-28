179bc21f9ead44b4f3a89e2919ac3fc540587e34
Alexander Chan <awc71403@gmail.com>
2021-03-28 14:03:24 -0700

    Merge conflict fix

--- Tile Turn-Based Party Project/Assets/Scripts/TileBehavior.cs
+++ Tile Turn-Based Party Project/Assets/Scripts/TileBehavior.cs
@@ -26,9 +26,9 @@ public abstract class TileBehavior : MonoBehaviour {
 
     #region Initialization
     void Awake() {
-        tileHighlighter.transform.position = transform.position;
-        tileHighlighterAnimator = tileHighlighter.GetComponent<Animator>();
-        setHighlightOpacity(playerOpacity);
+        //tileHighlighter.transform.position = transform.position;
+        //tileHighlighterAnimator = tileHighlighter.GetComponent<Animator>();
+        //setHighlightOpacity(playerOpacity);
     }
     #endregion
 
