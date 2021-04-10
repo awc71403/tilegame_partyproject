44bdb242dc64a4511cbacec9ecb66daa69c24de2
AdityaBhawal <33107769+AdityaBhawal@users.noreply.github.com>
2021-04-10 01:38:03 -0700

    Enemy movement framework

--- Tile Turn-Based Party Project/Assets/Scripts/Enemy.cs
+++ Tile Turn-Based Party Project/Assets/Scripts/Enemy.cs
@@ -8,6 +8,10 @@ public class Enemy : MonoBehaviour
     private Character myCharacter;
     private GameObject player;
 
+    private TileBehavior occupiedTile;
+    private string FLOOR = "floor";
+    private string WALL = "wall";
+
     public float viewDistance;
 
     void Start()
@@ -17,10 +21,9 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        DetectPlayer();
     }
 
-    void DetectPlayer()
+    bool DetectPlayer()
     {
         Vector2 direction = player.transform.position - transform.position;
         List<RaycastHit2D> results = new List<RaycastHit2D>();
@@ -47,7 +50,134 @@ public class Enemy : MonoBehaviour
             }
         }
 
-
-
+        return playerVisible;
     }
+
+    public void Turn()
+    {
+        DetectPlayer();
+        List<string> movementSteps = CalculateMovement(new List<string>(), originalTile, gameObject, unit.GetComponent<Character>().GetSpeed(), unitPlayer);
+        MoveInDirection(movementSteps[0]);
+    }
+
+    void MoveInDirection(string direction)
+    {
+        float tileSize = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+
+        if (direction.Equals("up"))
+        {
+            transform.position += new Vector3(0, tileSize);
+            TileBehavior upTile = occupiedTile.Up;
+            if (upTile != null && upTile.tileType != WALL && !upTile.HasUnit())
+            {
+                occupiedTile.ClearUnit();
+                occupiedTile = upTile;
+            }
+        }
+        else if (direction.Equals("right"))
+        {
+            transform.position += new Vector3(tileSize, 0);
+            TileBehavior rightTile = occupiedTile.Right;
+            if (rightTile != null && rightTile.tileType != WALL && !rightTile.HasUnit())
+            {
+                occupiedTile.ClearUnit();
+                occupiedTile = rightTile;
+            }
+        }
+        else if (direction.Equals("down"))
+        {
+            transform.position += new Vector3(0, -tileSize);
+            TileBehavior downTile = occupiedTile.Down;
+            if (downTile != null && downTile.tileType != WALL && !downTile.HasUnit())
+            {
+                occupiedTile.ClearUnit();
+                occupiedTile = downTile;
+            }
+        }
+        else if (direction.Equals("left"))
+        {
+            transform.position += new Vector3(-tileSize, 0);
+            TileBehavior leftTile = occupiedTile.Left;
+            if (leftTile != null && leftTile.tileType != WALL && !leftTile.HasUnit())
+            {
+                occupiedTile.ClearUnit();
+                occupiedTile = leftTile;
+            }
+        }
+        occupiedTile.PlaceUnit(myCharacter);
+    }
+
+
+
+
+    public List<string> CalculateMovement(List<string> movement, GameObject currentTile, GameObject goalTile, int moveEnergy, int unitPlayer)
+    {
+        // If you're there, return the movement path.
+        if (currentTile.Equals(goalTile))
+        {
+            return movement;
+        }
+
+        // If you're out of energy, it's an invalid path.
+        if (moveEnergy < 0)
+        {
+            return null;
+        }
+
+        List<List<string>> validPaths = new List<List<string>>();
+        // Check for all adjacent tiles:
+        Vector2[] directions = { Vector2.right, Vector2.left, Vector2.up, Vector2.down };
+        foreach (Vector2 direction in directions)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(currentTile.transform.position, direction, 1.0f);
+            if (hit.collider != null && hit.transform.GetComponent<TileBehavior>().tileType != "wall")
+            {
+                GameObject otherTileUnit = hit.transform.GetComponent<TileBehavior>().myUnit;
+                if (otherTileUnit == null || otherTileUnit.GetComponent<Character>().player == unitPlayer)
+                {
+                    List<string> newMovement = new List<string>(movement.ToArray());
+                    if (direction.Equals(Vector2.right))
+                    {
+                        newMovement.Add("right");
+                    }
+                    if (direction.Equals(Vector2.left))
+                    {
+                        newMovement.Add("left");
+                    }
+                    if (direction.Equals(Vector2.up))
+                    {
+                        newMovement.Add("up");
+                    }
+                    if (direction.Equals(Vector2.down))
+                    {
+                        newMovement.Add("down");
+                    }
+                    List<string> path = CalculateMovement(newMovement, hit.collider.gameObject, goalTile, moveEnergy - currentTile.GetComponent<TileBehavior>().movementCost, unitPlayer);
+                    if (path != null)
+                    {
+                        validPaths.Add(path);
+                    }
+                }
+            }
+        }
+
+        // Return the shortest valid path
+        if (validPaths.Count != 0)
+        {
+            List<string> shortestList = validPaths[0];
+            foreach (List<string> path in validPaths)
+            {
+                if (path.Count < shortestList.Count)
+                {
+                    shortestList = path;
+                }
+            }
+            return shortestList;
+        }
+
+        // If there are no valid paths from this point, return null
+        return null;
+    }
+
+
 }
--- Tile Turn-Based Party Project/Assets/Scripts/GameManager.cs
+++ Tile Turn-Based Party Project/Assets/Scripts/GameManager.cs
@@ -65,7 +65,11 @@ public class GameManager : MonoBehaviour
 
     public static void UpdateEnemies()
     {
-
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for(int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].GetComponent<Enemy>().Turn();
+        }
     }
 
     #region Set Up
