9df9417e4f60d0b0f2ca70bc8b3b6dbfe2abc5e9
Alexander Chan <awc71403@gmail.com>
2021-04-21 21:47:33 -0700

    Added unit prefabs

--- Tile Turn-Based Party Project/Assets/Scripts/PlayerManager.cs
+++ Tile Turn-Based Party Project/Assets/Scripts/PlayerManager.cs
@@ -90,19 +90,19 @@ public class PlayerManager : MonoBehaviour
             }
             
             else if (Input.GetKey(KeyCode.LeftShift)) {
-                if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.A)) {
+                if ((Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.A))) {
                     Debug.Log("upleft");
                     StartCoroutine(MoveUnitInDirection("upleft"));
                 }
-                if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.D)) {
+                if ((Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.D)) || (Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.D))) {
                     Debug.Log("upright");
                     StartCoroutine(MoveUnitInDirection("upright"));
                 }
-                if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) {
+                if ((Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.S) && Input.GetKey(KeyCode.A))) {
                     Debug.Log("downleft");
                     StartCoroutine(MoveUnitInDirection("downleft"));
                 }
-                if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)) {
+                if ((Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.D)) || (Input.GetKeyDown(KeyCode.S) && Input.GetKey(KeyCode.D))) {
                     Debug.Log("downright");
                     StartCoroutine(MoveUnitInDirection("downright"));
                     }
