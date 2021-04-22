2dd8616a6019177d32bf53bed108d8db56810345
Alexander Chan <awc71403@gmail.com>
2021-04-21 20:07:42 -0700

    Kathleen push

--- Tile Turn-Based Party Project/Assets/Scripts/RoomGenerator.cs
+++ Tile Turn-Based Party Project/Assets/Scripts/RoomGenerator.cs
@@ -40,22 +40,27 @@ public class RoomGenerator : MonoBehaviour
             {
                 int spawnrand = Random.Range(0, Handle.LRooms.Length);
                 bloc = Instantiate(Handle.LRooms[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("left room");
             }
             else if (direction == 1)
             {
                 int spawnrand = Random.Range(0, Handle.RRooms.Length);
                 bloc = Instantiate(Handle.RRooms[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("right room");
             }
             else if (direction == 2)
             {
                 int spawnrand = Random.Range(0, Handle.URooms.Length);
                 bloc = Instantiate(Handle.URooms[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("top room");
             }
             else
             {
                 int spawnrand = Random.Range(0, Handle.BRooms.Length);
                 bloc = Instantiate(Handle.BRooms[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("bot room");
             }
+            
         }
         else
         {
@@ -63,26 +68,31 @@ public class RoomGenerator : MonoBehaviour
             {
                 int spawnrand = Random.Range(0, Handle.LHall.Length);
                 bloc = Instantiate(Handle.LHall[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("left hall");
             }
             else if (direction == 1)
             {
                 int spawnrand = Random.Range(0, Handle.RHall.Length);
                 bloc = Instantiate(Handle.RHall[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("right hall");
             }
             else if (direction == 2)
             {
                 int spawnrand = Random.Range(0, Handle.UHall.Length);
                 bloc = Instantiate(Handle.UHall[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("top hall");
             }
             else
             {
                 int spawnrand = Random.Range(0, Handle.BHall.Length);
                 bloc = Instantiate(Handle.BHall[spawnrand], transform.position, Quaternion.identity);
+                Debug.Log("bot hall");
             }
+           
         }
 
-        Handle.RoomSize -= 1;
 
+        Handle.RoomSize -= 1;
         Destroy(this.gameObject);
 
     }
